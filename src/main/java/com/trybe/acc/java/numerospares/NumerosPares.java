0122fffff527096d523b3d4dd2a5e63cc8f063a2
Marcos Albuquerque <desenvolvedorwebfrontend@gmail.com>
2022-07-30 14:16:54 -0300

    Initial commit.

--- src/main/java/com/trybe/acc/java/numerospares/NumerosPares.java
+++ src/main/java/com/trybe/acc/java/numerospares/NumerosPares.java
@@ -11,7 +11,9 @@ public class NumerosPares {
    * 
    */
   public static void main(String[] args) {
-
+    for (int i = 4; i <= 20; i = i + 2) {
+      System.out.println(i);
+    }
   }
 
   /**
